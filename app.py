09a7138a032d9187ca48c9c3030afb2196638d54
clarestar <bernardhilary507@gmail.com>
2024-02-15 14:26:20 -0500

    Initial setup

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.run('localhost', 8081)
-
+    app.run(debug=True, port=8081)
+    
 from flask import Flask
 import random
 
